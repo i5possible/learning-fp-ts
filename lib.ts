f92121d9005c0130fa8dc162d86cedc09cb9e6f8
hong-liang <lianghongbuaa@gmail.com>
2021-09-15 14:05:24 +0800

    add lib

--- lib.ts
+++ lib.ts
@@ -4,7 +4,8 @@ import * as T from "fp-ts/Task";
 import * as TE from "fp-ts/TaskEither";
 import * as A from "fp-ts/Array";
 import * as NEA from "fp-ts/NonEmptyArray";
+import * as J from "fp-ts/lib/Json";
 import { flow, pipe } from "fp-ts/function";
 import Do from "fp-ts-contrib/Do";
 
-export { O, E, T, TE, A, NEA, flow, pipe, Do };
+export { O, E, T, TE, A, NEA, flow, pipe, Do, J };
