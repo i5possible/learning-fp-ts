c69c9801ee1fc5122584998faa1409c55f9c05f7
hong-liang <lianghongbuaa@gmail.com>
2021-09-16 13:45:15 +0800

    update taskEither example

--- TaskEither.ts
+++ TaskEither.ts
@@ -1,4 +1,4 @@
-import { E, O, pipe } from "./lib";
+import { E, pipe, T, TE } from "./lib";
 
 // Constructors
 
@@ -9,5 +9,35 @@ import { E, O, pipe } from "./lib";
 // Others functions
 
 // Examples
+type Item = {
+  id: string;
+  name: string;
+};
+
+// create function return TE.left and TE.right for these two type
+type ItemIdsByList = (listId: string) => TE.TaskEither<string, string[]>;
+type ItemDetailById = (itemId: string) => TE.TaskEither<string, Item>;
+
+const fetchItemIdsByListRight = (
+  listId: string
+): TE.TaskEither<string, string[]> => TE.right(["1", "2", "3"]);
+const fetchItemDetailByIdRight = (listId) => TE.left("There is no such list!");
+
+const fetchItems =
+  (fetchItemIdsByList: ItemIdsByList, fetchItemDetailById: ItemDetailById) =>
+  (listId): T.Task<string> =>
+    pipe(
+      listId,
+      E.fromNullable("The listId is not present!"),
+      TE.fromEither,
+      TE.fold(
+        (left) => T.of(left),
+        (right) => T.of(`The items are ${JSON.stringify(right)}`)
+      )
+    );
+
+console.log(
+  fetchItems(fetchItemIdsByListRight, fetchItemDetailByIdRight)("1")()
+);
 
 // Tasks
