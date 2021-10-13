a1850e48f8f85bb0b04e0f6c3bb04c31f00a41ee
hong-liang <lianghongbuaa@gmail.com>
2021-10-13 16:36:37 +0800

    update lib

---
+++ src/lib.ts
@@ -0,0 +1,13 @@
+import * as O from "fp-ts/Option";
+import * as E from "fp-ts/Either";
+import * as T from "fp-ts/Task";
+import * as TE from "fp-ts/TaskEither";
+import * as TH from "fp-ts/These";
+import * as A from "fp-ts/Array";
+import * as RA from "fp-ts/ReadonlyArray";
+import * as NEA from "fp-ts/NonEmptyArray";
+import * as J from "fp-ts/lib/Json";
+import { flow, pipe } from "fp-ts/function";
+import Do from "fp-ts-contrib/Do";
+
+export { O, E, T, TE, TH, A, RA, NEA, flow, pipe, Do, J };
