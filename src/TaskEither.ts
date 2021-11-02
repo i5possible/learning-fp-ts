6525df3fd9fb4802921e5518b1510bf09de4d081
hong-liang <lianghongbuaa@gmail.com>
2021-11-02 16:45:28 +0800

    update TaskEither examples and tests

--- src/TaskEither.ts
+++ src/TaskEither.ts
@@ -1,43 +1,109 @@
-import { E, pipe, T, TE } from "./lib";
+import { string } from "fp-ts";
+import { E, pipe, T, TE, A, O } from "./lib";
 
 // Constructors
+const taskTE = TE.of("task");
+const leftTE: TE.TaskEither<string, string> = TE.left<string, string>("left");
+const rightTE = TE.right("right");
 
 // Transformers
+TE.map((value: string) => value.length)(taskTE);
+TE.map((value: string) => value.length)(leftTE);
+pipe(
+  taskTE,
+  TE.chain((value: string) => TE.of(value.length))
+);
+pipe(
+  leftTE,
+  TE.chain((value: string) => TE.of(value.length))
+);
+TE.getOrElse(() => T.of("some task"))(taskTE);
+TE.getOrElse(() => T.of("some task"))(leftTE);
+TE.filterOrElse(
+  (value) => value === "task",
+  () => TE.left("not a task")
+);
 
 // De-constructors
+pipe(
+  taskTE,
+  TE.fold(
+    () => T.of("It's a left"),
+    (right) => T.of(right)
+  ),
+  (task) => task()
+);
 
 // Others functions
+TE.sequenceArray;
+TE.traverseArray;
 
 // Examples
 type Item = {
-  id: string;
+  id: number;
   name: string;
+  description: string;
 };
 
 // create function return TE.left and TE.right for these two type
-type ItemIdsByList = (listId: string) => TE.TaskEither<string, string[]>;
-type ItemDetailById = (itemId: string) => TE.TaskEither<string, Item>;
+type ItemIdByName = (itemName: string) => TE.TaskEither<string, number>;
+type ItemDetailById = (itemId: number) => TE.TaskEither<string, Item>;
 
-const fetchItemIdsByListRight = (
-  listId: string
-): TE.TaskEither<string, string[]> => TE.right(["1", "2", "3"]);
-const fetchItemDetailByIdRight = (listId) => TE.left("There is no such list!");
+const itemNames = [
+  { id: 1, name: "firstItem" },
+  { id: 2, name: "secondItem" },
+  { id: 2, name: "itemThree" },
+];
 
-const fetchItems =
-  (fetchItemIdsByList: ItemIdsByList, fetchItemDetailById: ItemDetailById) =>
-  (listId): T.Task<string> =>
-    pipe(
-      listId,
-      E.fromNullable("The listId is not present!"),
-      TE.fromEither,
-      TE.fold(
-        (left) => T.of(left),
-        (right) => T.of(`The items are ${JSON.stringify(right)}`)
-      )
-    );
+const itemDetails: Item[] = [
+  {
+    id: 2,
+    description: "Some description about this item2",
+    name: "secondItem",
+  },
+  {
+    id: 3,
+    description: "Some description about this item3",
+    name: "itemThree",
+  },
+  {
+    id: 4,
+    description: "Some description about this item4",
+    name: "itemFour",
+  },
+];
 
-console.log(
-  fetchItems(fetchItemIdsByListRight, fetchItemDetailByIdRight)("1")()
+const fetchItemIdByName = (itemName: string): TE.TaskEither<string, number> =>
+  pipe(
+    itemNames,
+    A.findFirst((item) => item.name === itemName),
+    O.map((item) => item.id),
+    E.fromOption(() => "Item does not exist!"),
+    TE.fromEither
+  );
+
+const fetchItemDetailById = (itemId: number): TE.TaskEither<string, Item> =>
+  pipe(
+    itemDetails,
+    A.findFirst((item) => item.id === itemId),
+    E.fromOption(() => "Item detail is missing!"),
+    TE.fromEither
+  );
+
+const fetchItem =
+  (fetchItemIdByName: ItemIdByName, fetchItemDetailById: ItemDetailById) =>
+  (itemName: string): TE.TaskEither<string, Item> =>
+    pipe(itemName, fetchItemIdByName, TE.chain(fetchItemDetailById));
+
+export const fetchItemByName = fetchItem(
+  fetchItemIdByName,
+  fetchItemDetailById
 );
 
 // Tasks
+// implement the following two functions
+type itemIdsByListId = (listId: number) => number[];
+
+export declare function fetchItemsByListId(
+  listId: number
+): TE.TaskEither<string, Item[]>;
